c96f20271e793dd98811a11ae4e2abfeb5b791b7
BlenderIsaac <69485844+BlenderIsaac@users.noreply.github.com>
2025-02-25 08:46:46 +1300

    Slerpy camera, alright

--- Levels/Cam.gd
+++ Levels/Cam.gd
@@ -32,7 +32,7 @@ var begin_transform_override = false:
 @onready var pull_back = $PullBack
 
 var camera_velocity = Vector3()
-var direction = Vector3()
+var direction = Vector3(0, 0, 0)
 
 func _process(_delta):
 	
@@ -156,101 +156,37 @@ func _process(_delta):
 		
 		pull_back.target_position = backaway
 		
-		if !current:
-			DebugDraw3D.draw_position(Transform3D(Basis(), pos), Color.RED)
-			DebugDraw3D.draw_position(Transform3D(Basis(), backaway), Color.RED)
-			DebugDraw3D.draw_position(Transform3D(Basis(), position), Color.ORANGE)
-			DebugDraw3D.draw_camera_frustum(self, Color.GREEN)
-		
-		var target = Vector3()
+		#if !current:
+			#DebugDraw3D.draw_position(Transform3D(Basis(), pos), Color.RED)
+			#DebugDraw3D.draw_position(Transform3D(Basis(), backaway), Color.RED)
+			#DebugDraw3D.draw_position(Transform3D(Basis(), position), Color.ORANGE)
+			#DebugDraw3D.draw_camera_frustum(self, Color.GREEN)
 		
 		DebugDraw2D.set_text("Distance 2D", dist2d)
+		DebugDraw2D.set_text("Move", "Stay")
 		
 		var follow_dist = 5.0
 		var back_off_dist = 3.0
-		var accel = 0.0001
+		var accel = 0.1
 		
-		var accel2 = 0.1
-		
-		DebugDraw2D.set_text("Move", "Stay")
-		DebugDraw2D.set_text("Camera Velocity", camera_velocity)
-		
-		
-		#DebugDraw3D.draw_position(Transform3D(Basis(), position), Color.RED, 3.0)
-		
-		DebugDraw3D.draw_sphere(target_agent.get_final_position(), follow_dist)
 		var velocity_to = Vector3()
 		
 		if dist2d > follow_dist:
 			DebugDraw2D.set_text("Move", "Follow!")
-			camera_velocity += -(position-target_agent.get_next_path_position()).normalized()*accel*dist2d
 			
-			velocity_to = -(position-target_agent.get_next_path_position())*accel2#*dist2d
+			velocity_to = -(position-target_agent.get_next_path_position()).normalized()*accel
 		elif dist2d < back_off_dist:
 			DebugDraw2D.set_text("Move", "Back Off, Tony!")
-			camera_velocity += -(position-pull_back.get_next_path_position()).normalized()*accel*dist2d
 			
-			velocity_to = -(position-pull_back.get_next_path_position()).normalized()*accel2#*dist2d
+			velocity_to = -(position-pull_back.get_next_path_position()).normalized()*accel
 		else:
-			camera_velocity = Vector3()#camera_velocity.lerp(Vector3(), accel*_delta*60)
 			velocity_to = Vector3()
 		
-		#camera_velocity = Vector3()
+		direction = direction.slerp(velocity_to, _delta*3)
 		
-		global_position += velocity_to
-		#global_position += camera_velocity
-		movement_to = 1.0
-		#var min_dist = 5.0
-		#var pushaway_dist = 4.0
-		
-		#var should_move = false
-		
-		#if final_vec2_dist < pushaway_dist:
-		
-		
-		#var move_pos = Vector3()
-		#DebugDraw2D.set_text("Pushing", false)
-		#if final_vec2_dist < pushaway_dist:
-			#DebugDraw2D.set_text("Pushing", true)
-			
-		#	var pull_back_pos = pull_back.get_next_path_position()# + Vector3(0, -0.3, 0)
-		#	var back_vec2_dist = f.to_vec2(global_position).distance_to(f.to_vec2(pull_back.get_final_position()))
-			
-		#	pull_back.target_position = backaway
-			
-		#	var speed = (back_vec2_dist-pushaway_dist)
-			#DebugDraw2D.set_text("speed", speed)
-			#DebugDraw3D.draw_position(Transform3D(Basis(), pull_back_pos), Color.BLUE)
-			
-		#	move_pos += (pull_back_pos-global_position).normalized()*minf(speed/30, 3.0)
-		
-		
-		#if final_vec2_dist > min_dist:
-			
-		#	var next_pos = target_agent.get_next_path_position()# + Vector3(0, -0.3, 0)
-			
-		#	var speed = (final_vec2_dist-min_dist)
-		#	move_pos += (next_pos-global_position).normalized()*minf(speed/30, 3.0)#(next_pos-global_position).normalized()*minf(speed/30, 3.0)*movement_at
-		
-		
-		#DebugDraw2D.set_text("Move Pos", move_pos.length())
-		#DebugDraw2D.set_text("Movement To", movement_to)
-		#DebugDraw2D.set_text("Movement At", movement_at)
-		#if move_pos.length() > 0.001:
-		#	movement_to = 1.0
-		#else:
-		#	movement_to = 0.0
-		
-		#global_position += move_pos#*movement_at
-		
-		#elif final_vec2_dist < 3.0:
-			#var next_pos = target_agent.get_next_path_position() + Vector3(0, -0.3, 0)
-			#
-			#if final_vec2_dist > 0.2:
-				#
-				#var speed = (final_vec2_dist-3.0)
-				#global_position += (next_pos-global_position).normalized()*minf(speed/30, 3.0)
-		
+		var speed = 1
+		global_position += direction*speed
+		movement_at = 1.0
 		
 		rotation.z = 0
 		if f.to_vec2(global_position-avg) == Vector2():
@@ -264,7 +200,7 @@ func _process(_delta):
 	
 	start_timer += 1
 
-func path_length(vector_array):
+func get_path_length(vector_array):
 	var path_length = 0
 	for vector in vector_array:
 		path_length += vector.length()
